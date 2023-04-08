817600963db372ec69fd3d1cded37203120a2ad3
Nomonzon <nomonzonrh@gmail.com>
2023-04-09 00:08:37 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+lists = ["Nomonjon", "Temur", "Feruz", "Komil", "To'ychi", "Shukur", "Daler", "Aydos"]
+print("<<<<Before sorting>>>>")
+print(lists)
+lists.sort()
+print("<<<<After sorting>>>>")
+print(lists)
